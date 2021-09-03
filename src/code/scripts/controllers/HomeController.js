65208451bb4b5e8c8a7f603f859f9a85c980876b
t <t@t.com>
2021-09-03 14:25:14 +0100

    refs #1 - stable integration with epi

--- src/code/scripts/controllers/HomeController.js
+++ src/code/scripts/controllers/HomeController.js
@@ -1,7 +1,29 @@
 const {WebcController} = WebCardinal.controllers;
 
+/**
+ * https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+ * @param query
+ * @returns {*}
+ */
+const getQueryStringParams = () => {
+    const query = window.frameElement.src;
+    return query
+        ? (/^[?#]/.test(query) ? query.slice(1) : query)
+            .split('&')
+            .reduce((params, param) => {
+                    let [key, value] = param.split('=');
+                    params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+                    return params;
+                }, {}
+            )
+        : {}
+};
+
 export default class HomeController extends WebcController{
-    constructor(...args) {
-        super(...args);
+    constructor(element, history, ...args) {
+        super(element, history, ...args);
+        const gs1Data = getQueryStringParams();
+        console.log(window, gs1Data);
+
     }
 }
